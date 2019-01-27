77ab24a12adcba17ed34d9a11c79750bceb92489
Alexandre Calil Blasizza Paravani <alexandre.paravani@gruponewway.com.br>
2019-01-27 07:37:00 -0200

    adicionado as pasta do projeto

---
+++ js/allData.js
@@ -0,0 +1,62 @@
+// -- MODELO --
+/*
+{ 
+    id: '',
+    name: '',
+    className:'',
+    specialty: '',
+    life: '',
+    defense: '',
+    damage: '',
+    attackSpeed: '',
+    moveSpeed: '',
+}
+*/
+
+var characters = [
+    
+    /* char #01 */
+    {
+        id: 0,
+        name: 'Steven',
+        className:'Mago',
+        specialty: 'Magia Branca',
+        life: '2900',
+        defense: '200',
+        damage: '340',
+        attackSpeed: '1.3',
+        moveSpeed: '320',
+        thumbnail: 'thumb'
+    },
+    
+    /* char #02 */
+    {
+        id: 1,
+        name: 'Mona',
+        className:'Sacerdote',
+        specialty: 'Cura, Magia Branca',
+        life: '3100',
+        defense: '200',
+        damage: '180',
+        attackSpeed: '1.3',
+        moveSpeed: '330',
+        thumbnail: 'thumb'
+    },
+    
+    /* char #03 */
+    {
+        id: 2,
+        name: 'Morgan',
+        className:'Lutador',
+        specialty: 'Magia Branca',
+        life: '6000',
+        defense: '360',
+        damage: '130',
+        attackSpeed: '1.1',
+        moveSpeed: '300',
+        thumbnail: 'thumb'
+    }
+    
+]
+    
+    

dd0bce82f2b78dc15208ddce821e7ef9f45c2507
Alexandre Calil Blasizza Paravani <alexandre.paravani@gruponewway.com.br>
2019-01-31 16:34:32 -0200

    utimos ajustes e comentários no código

--- js/controllers/createController.js
+++ js/controllers/createController.js
@@ -5,31 +5,40 @@
 // --------------------------------------------------------------------------- //
 
 
-challenge.controller('createCtrl', ['$scope','$http', '$location', '$routeParams', function ($scope, $http, $location, $routeParams ) {
+challenge.controller('createCtrl', ['$scope','$http', '$location', '$routeParams','$rootScope', function ($scope, $http, $location, $routeParams,$rootScope ) {
                                         
-    /* -- Variaveis -- */   
-                                        
-    var url    = $location.url();
-    var create = (url == '/Create');
-    var id     = $routeParams.id;
-    var classID  = 0;
-                                        
+    // URL
+     var url = {
+        local : $location.url(),                    // Pega a url da rota atual
+        API   : "http://heroes.qanw.com.br:8674",   // url da API
+        id    : $routeParams.id                     // Pega os parametros da rota
+    }
+    
+    var classID  = 0;                               // Salva o id da classe que foi selecionada
+    var create   = ( url.local == '/Create' );      // Verifica se o usuario quer editar ou criar um heroi
+    
+    
+    /*  --------------------------------------------------------------------------------------------
+        Para fazer a lista com as imagens já preexistente, eu precisaria que a API retornasse essa lista para mim, porém não exite função, por isso que salvei nesse array todos os IDs das imagens que foram salvas.
+        (todas a imagens eu salvei pelo Postman)
+        --------------------------------------------------------------------------------------------  */
+    var photosID = [25,26,27,28,29,30,31,32,33,34];
+    
   
     
 //---------------------------------------------------------------------------
         
     /* -- ngBind -- */
     
-    $scope.disabledScreen = false;
-                                        
-    $scope.title        = create ? "Novo Herói": "Editar Herói";
-
-    $scope.activeImage  = {};
-    $scope.activeClass  = 'form__image_active';
-    $scope.listImage    = photosID;
-    $scope.url          = "http://heroes.qanw.com.br:8674";
-    $scope.urlPhotos    = $scope.url + "/photos/";
+    $scope.title        = create ? "Novo Herói": "Editar Herói"; // Titulo da tela 
     
+    $scope.listImage    = photosID;                 // Array de IDs das imagens
+    $scope.activeImage  = {};                       // Imagens selecionadas
+    $scope.activeClass  = 'form__image_active';     // Classe correspondente as imagens selecioandas
+    $scope.urlPhotos    = url.API + "/photos/";     // URL das imagens
+    
+    
+    // Heroi: objeto padrão
     $scope.heroe        = {
         id              : null,
         class_id        : null,
@@ -42,166 +51,183 @@ challenge.controller('createCtrl', ['$scope','$http', '$location', '$routeParams
         class_name      : "",
         specialties     : [],
         photos          : []
-    };
-    $scope.specialties  = [];
-    $scope.classes      = [];
-                                        
-    $scope.flagSpecialties  = [];
-    $scope.titleSpecialties = "Selecione uma ou mais opções";
-    $scope.currClasses      = null;
+    }; 
+    $scope.specialties  = [];       // Lista de especialidades
+    $scope.classes      = [];       // Lista de classes
+           
+    
+    $scope.titleSpecialties = "Selecione uma ou mais opções";   // Titulo do botão especialidade
+    $scope.currClasses      = null;                             // Classe escolhida
     
                              
  //---------------------------------------------------------------------------   
     
     
+    /* Inicia junto com a TELA */
     $scope.init = function  (){
         
-        
+        // Por definição, deixa todas a imagens sem a marcação de ativas
         for (var i=0; i< photosID.length; i++){
             $scope.activeImage[photosID[i]] = '';
         }
         
+        //------------------------------------------------------------
+        
         $scope.getHeroe();
         $scope.getSpecialties();
         $scope.getClasses();
-        
-        
     }
         
    
-                                        
+    /* Pega o heroi que o usuraio gostaria de editar */                                    
     $scope.getHeroe = function (_test){
         
+        //caso seja ele queria criar, essa função deixa de ser executada
         if (create) return;
         
-    //------------------------------------------------------------
+        //------------------------------------------------------------
         
-        var http        = {};
-        var result      = [];
-        var i = 0;
-        var idImage = 0;
+        var i       = 0;    // contador usado no for
+        var imageID = 0;    // referência o id das imagens que já foram selecionadas pelo usuario
+        
+        var _http   = { method : "GET", url : url.API+"/heroes/"+url.id };
+        
+        //------------------------------------------------------------
         
         
-    //------------------------------------------------------------
-        
-        
-        http = { method : "GET", url : $scope.url+"/heroes/"+id };
-        
-        $http(http).then(
+        // Requisição para pegar os dados do Heroi
+        $http(_http).then(
 
             function mySuccess(response) {
-                $scope.heroe = response.data;
-                classID      = $scope.heroe.class_id;
                 
+                $scope.heroe = response.data;           // salva o resultado retornado
+                classID      = $scope.heroe.class_id;   // salva o id da classe do heroi
                 
-                 for ( i = 0; i< $scope.heroe.photos.length; i++){
+                console.log($scope.heroe.photos);
+                // varre a lista de fotos do heroi, para assim deixa-las como checkadas na dela 
+                for ( i = 0; i< $scope.heroe.photos.length; i++){
                      
-                     idImage = $scope.heroe.photos[i];
-                    $scope.activeImage[idImage] =  $scope.activeClass;
+                     imageID = $scope.heroe.photos[i];                      // guarda os ids das fotos
+                     $scope.activeImage[imageID] =  $scope.activeClass;     // deixa em destaque as fotos já selecionadas
                 }
-//                
+                
             }, 
 
             function myError(response) {
-                //$scope.charList = response.statusText;
+                alert("Não foi possivel carregar os dados desse heroi");
             }
         );
     }
     
     
+    /* Pega todas as epecialidades */  
     $scope.getSpecialties = function (){
-        http = { method : "GET", url : $scope.url+"/specialties" };
         
-        var _scopeS  = [];
-        var _scopeHS = $scope.heroe.specialties;
+        var _http = { method : "GET", url : url.API+"/specialties" };
         
-
-        $http(http).then(
+        // Requisição para pegas as Especialidades
+        $http(_http).then(
 
             function mySuccess(response) {
                 
-                $scope.specialties = response.data;
-                
-                $scope.changeEspecialities(null);
-                
-                
+                $scope.specialties = response.data; // salva o resultado retornado
+                $scope.changeSpecialities(null);    // deixa checkado todas as especialidades que o heroi já possuia
             }, 
 
             function myError(response) {
-                console.log(response.statusText);
+                //Informa que os dados Não foram carregados
+                alert("Não foi possivel carregar as Especialidades");
             }
         );
     }
     
     
+    /* Pega todas as classes */  
     $scope.getClasses = function (){
-        http = { method : "GET", url : $scope.url+"/classes" };
+        
+        var _http = { method : "GET", url : url.API+"/classes" };
 
-        $http(http).then(
+        // Requisição para pegas as classes
+        $http(_http).then(
 
             function mySuccess(response) {
                 
-                $scope.classes = response.data;
+                $scope.classes = response.data;     // salva o resultado retornado
                 
+                // Varre toas as classes para verificar qual delas pertencem ao heroi
                 for (var i=0; i< $scope.classes.length; i++){
                     
+                    // Verifica qual é a classe atual
                     if ($scope.classes[i].id == classID)
-                        $scope.currClasses = $scope.classes[i];
+                        $scope.currClasses = $scope.classes[i];  // Salva a referencia da classe atual 
                 }
-
             },
             
             function myError(response) {
-                console.log(response.statusText);
+                
+                //Informa que os dados NÂO foram carregados
+                alert("Não foi possivel carregar as classes");
+                
             }
         );
     }
     
     
-    
+     /* Impede que o box do dropdown (Especialidade) suma quando for escolhido açguma alternativa  */ 
     $scope.dropdownMenu = function (e){
         e.stopPropagation();
     }
     
     
-    $scope.changeEspecialities = function (obj, index, e){
+    /* Ativa ou desativa a especialidade clicada  */ 
+    $scope.changeSpecialities = function (obj, index, e){
         
-        var virgula = ", ";
-        var count = 0;
-        var flag = true;
-        var teste = "";
+        var _spc            = $scope.specialties;           // salva a referencia da lista de especialidades
+        var _heroSpc        = $scope.heroe.specialties;     // salva a referencia da lista de especialidades do heroi
         
-        var specialties  = $scope.specialties;
-        var specialtiesH = $scope.heroe.specialties;
+        var _spcLength      = $scope.specialties.length;            // tamanho da lista de especialidades
+        var _heroSpcLength  = $scope.heroe.specialties.length;      // tamanho da lista de especialidades do heroi
         
-        var lengthI = $scope.heroe.specialties.length;
-        var lengthJ = $scope.specialties.length;
-        
-        
+                
+        // Caso a função tenha sido chamada ao iniciar a tela
         if (obj == null) {
             
-            for (var i = 0; i < lengthI ; i++)
-            for (var j = 0; j < lengthJ ; j++) {
+            // Varre as duas listas para poder adicionar o "check" nas especialidades que o heroi já possui 
+            for (var i = 0; i < _heroSpcLength ; i++)
+            for (var j = 0; j < _spcLength     ; j++) {
 
-                if (specialties[j].id == specialtiesH[i].id){
-                    specialties[i].check = true;
+                // verifica se o heroi já apossui essa especialidade 
+                if (_spc[j].id == _heroSpc[i].id){
+                    _spc[i].check = true;
                 }
             }
         }
         else {
-            specialties[index].check = !specialties[index].check;
-            console.log(specialties[index]);
+            // apenas inverte o check da especialidade clicada
+            _spc[index].check = !_spc[index].check;
         }
+        
     }
     
     
+    /* Ativa ou desativa a imagem clicada */ 
     $scope.selectedImage = function (_id, index){
         $scope.activeImage[_id] = ($scope.activeImage[_id] == "") ? $scope.activeClass : "";
     }
     
     
+    /* CANCELAR */ 
     $scope.cancel = function (){
-        $scope.heroe        = {
+        
+        var _confirm = confirm("Tem certeza que você quer sair sem salvar?");
+        
+        // Verifica se o usuario quer mesmo cancelar a criação/edição do heroi
+        if (_confirm == false)  { 
+            return;
+        }
+        
+        // Zera as informações do heroi
+        $scope.heroe = {
             id              : null,
             class_id        : null,
             name            : '',
@@ -215,22 +241,34 @@ challenge.controller('createCtrl', ['$scope','$http', '$location', '$routeParams
             photos          : []
         };
         
+        // Redirenciona para a pagina inicial
         $location.path('/');
     }
     
     
+    /* SALVAR */
     $scope.save = function (){
         
-        $scope.disabledScreen = true;
+        // Ativa a div para impedir que o usuario clique em alguma coisa enquanto está sendo salvo as informações
+        $('.background-loading').css('display', 'block');
         
+        // Mensagens do alert
+        var _alert = {
+            success : create ? "O heroi foi criado com sucesso"    : "O heroi foi editado com sucesso",
+            error   : create ? "Não foi possivel criar esse heroi" : "Não foi possivel editar esse heroi"
+        }
+        
+        // Retorna somente os IDs das Especialidades selecionadas
         var _specialties = $scope.specialties.filter(function(f){  return f.check })
                                              .map   (function(e){  return e.id;   });
         
-        var _image       = Object.keys   ($scope.activeImage)
-                                 .filter (function(f){  return $scope.activeImage[f] })
-                                 .map    (function(e){  return parseInt(e);   });
+        // Retorna somente os IDs das Classes selecionadas
+        var _image = Object.keys   ( $scope.activeImage)
+                           .filter ( function(f){  return $scope.activeImage[f] })
+                           .map    ( function(e){  return parseInt(e);          });
         
-        var _heroe        = {
+        // Guarda as informações do heroi que serão salvas
+        var _heroe = {
             id              : $scope.heroe.id,
             class_id        : $scope.currClasses.id,
             name            : $scope.heroe.name,
@@ -245,34 +283,34 @@ challenge.controller('createCtrl', ['$scope','$http', '$location', '$routeParams
         };
         
         
-        
-        if (create) {
-             http = { method : "POST", url : $scope.url+"/heroes", data:_heroe };
-        }
-        else {
-            http = { method : "PUT", url : $scope.url+"/heroes/"+id, data:_heroe };
-        }
+        // Se estiver na tela de criação, então será feito um POST, se não, será feito um PUT
+        if (create) { http = { method : "POST", url : url.API + "/heroes"         , data:_heroe }; }
+        else        { http = { method : "PUT" , url : url.API + "/heroes/"+url.id , data:_heroe };  }
         
         
+        //Requisição para salvar os dados
         $http(http).then(
 
             function mySuccess(response) {
-                console.log('mySucces ', response.statusText);
+                
+                //Informa que os dados foram salvos
+                alert(_alert.success);
+                
+                // Desativa a div para impedir que o usuario clique em alguma coisa enquanto está sendo salvo as informações
+                $('.background-loading').css('display', 'none');
+                
+                // Redirenciona para a pagina inicial
                 $location.path('/');
-
-                $scope.disabledScreen = false;
             }, 
 
             function myError(response) {
-                console.log('myError', response.statusText);
-                $scope.disabledScreen = false;
+                
+                //Informa que os dados NÃO foram salvos
+                alert(_alert.error);
+                
             }
         );
-        
-//        $location.path('/');
     }
-    
-    
     
     
 }]);
--- js/controllers/dashboardController.js
+++ js/controllers/dashboardController.js
@@ -5,27 +5,27 @@
 // --------------------------------------------------------------------------- //
 
 
-challenge.controller('dashboardCtrl', ['$scope','$http', '$location', function ($scope, $http, $location) {
+challenge.controller('dashboardCtrl', ['$scope','$http', '$location','$rootScope', function ($scope, $http, $location, $rootScope) {
     
     
     /* -- Variaveis -- */
-    var contOrder = 0;
-    var lastOrder = "";
-    var url       = "http://heroes.qanw.com.br:8674";
+    var contOrder = 0;      // auxilia a manipular o ordenação
+    var lastOrder = "";     // salva a ultima ordenação utilizada
+    
+    var url       = "http://heroes.qanw.com.br:8674";  // url da API
     
 
 //---------------------------------------------------------------------------
     
     /* -- ngBind -- */
     
-    $scope.charList       = [];
-    $scope.nameTitle      = "id";
-    $scope.activeOrder    = "";
-    $scope.photo          = "";
-    
-    $scope.seachType      = "";
-    $scope.searchHeroe    = "";
+    $scope.charList       = [];     // lista de herois cadastrada
+    $scope.nameTitle      = "id";   // titulo de cada coluna da tabela
+    $scope.activeOrder    = "";     // ordenação atual
+    $scope.photo          = "";     // url da primeira foto da lista de fotos do heroi
+    $scope.searchHeroe    = "";     // filtro da busca
         
+    // Lista para popular os titulos da tabela
     $scope.attributeList = [
         {title: "Nome"              , orderName: "name"           },
         {title: "Classe"            , orderName: "class_name"     },
@@ -40,23 +40,29 @@ challenge.controller('dashboardCtrl', ['$scope','$http', '$location', function (
     
 //---------------------------------------------------------------------------
 
-   
+    /* Ordena a tabela */
     $scope.orderByFunc = function (order){
         
+        // Se nenhum titulo foi selecionado
         if (order == "") return;
         
-    //------------------------------------------------------------
         
+        // Se o titulo selecionado é diferento do ultimo selecionado
         if ( order != lastOrder ) {
             lastOrder          = order;
             $scope.nameTitle   = order;
             $scope.activeOrder = order;
             contOrder = 0;
         }
+        
+        // Se for igual
         else {
+            
+            // Coloca na ordem decrescente
             $scope.nameTitle = "-"+order;
             contOrder++;
             
+            // Desabilita o filtro
             if ( contOrder == 2 ) {
                 
                 contOrder        = 0;
@@ -70,27 +76,25 @@ challenge.controller('dashboardCtrl', ['$scope','$http', '$location', function (
     };
     
     
-    /**
-     *  GET HEROES
-     * */
-    $scope.getHeroes = function ( _test ) {
-        
+    /* Pega a lista de herois cadastrados */ 
+    $scope.getHeroes = function (  ) {
         
         var http        = {};
         var result      = [];
         var specialties = '';
-        var photos      = '';
-        var i           = 0;
+        var i           = 0;     // contador usado no for
     
      //------------------------------------------------------------
         
 
         http = { method : "GET", url : url+"/heroes" };
 
+        // Requisição para pegar a lista de herois cadastrados
         $http(http).then(
 
             function mySuccess(response) {
-
+                
+                // salva o resultado retornado
                 result = response.data;
 
                 //Leitura de cada item do objeto recebido
@@ -104,55 +108,87 @@ challenge.controller('dashboardCtrl', ['$scope','$http', '$location', function (
                         return item['name'];
                     }).join (", ");
                     
+                    
+                    // Salva a url das imagens
                     result[i].photos = url + "/photos/" + result[i].photos[0];
 
+                    // Salva as especialidade no objeto principal
                     result[i].specialties = specialties;
                 };
 
+                // salva result no charList
                 $scope.charList = result;
-                
-                console.log(result)
                 
             }, //FIM - função
 
             function myError(response) {
-                $scope.charList = response.statusText;
+                alert("Não foi possivel carregar a lista de herois");
             }
         );
 
-        
     };
-   
     
+   
+    /* Deleta da lista, o heroi selecionado */ 
     $scope.deleteHereo = function ( _id, _index ){
         
         
-        http = { method : "DELETE", url : url+"/heroes/"+_id };
+        // Ativa a div para impedir que o usuario clique em alguma coisa enquanto está sendo salvo as informações
+        $('.background-loading').css('display', 'block');
+        
+        var _confirm = confirm("Tem certeza que você quer excluir esse heroi?");
+        
+        // Mensagens dos alerts
+        var _alert = {
+            success : "O heroi foi excluido com sucesso",
+            error   : "Não foi possivel excluir esse heroi"
+        }
+        
+        // Verifica se o usuario quer mesmo excluir o heroi
+        if (_confirm == false)  { 
+            
+             // Desativa a div para impedir que o usuario clique em alguma coisa enquanto está sendo salvo as informações
+            $('.background-loading').css('display', 'none');
+            
+            return;
+        }
+        
+            
+        var _http = { method : "DELETE", url : url+"/heroes/"+_id };
 
-        $http(http).then(
+        $http(_http).then(
 
             function mySuccess(response) {
-                console.log('mySucces ', response.statusText);
                 
-                $scope.charList.splice(_index, 1);
+                //Informa que o heroi foi excluido
+                alert(_alert.success);
 
-                //mesagem de sucesso - response.statusText;
-            }, 
+                //Exclui da lista atual
+                $scope.charList.splice(_index, 1);
+                
+                // Desativa a div para impedir que o usuario clique em alguma coisa enquanto está sendo salvo as informações
+                $('.background-loading').css('display', 'none');
+            },
 
             function myError(response) {
-                console.log('myError ', response.statusText);
-                // mesagem de erro - response.statusText;
+                
+                //Informa que NÃO foi possivel excluir o heroi
+                alert(_alert.error);
+                
+                // Desativa a div para impedir que o usuario clique em alguma coisa enquanto está sendo salvo as informações
+                $('.background-loading').css('display', 'none');
             }
         );
-        
+            
     }
     
     
+    /* Redirenciona para a pagina de Edição */ 
     $scope.editHereo = function ( id ){
         $location.path('Edit/' + id);
     }
     
-    
+    /* Redirenciona para a pagina de Criação */ 
     $scope.createHereo = function ( ){
         $location.path('Create');
     }
